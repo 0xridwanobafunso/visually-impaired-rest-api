abf57e512983c022f40408ad849e255aba4a9809
Obafunso Ridwan Adebayo <104316701+0xridwanobafunso@users.noreply.github.com>
2022-06-23 18:33:14 +0100

    Update scores.js

--- src/routes/scores.js
+++ src/routes/scores.js
@@ -7,7 +7,7 @@ const router = express.Router()
 const mustBeAdmin = require('../middlewares/mustBeAdmin')
 
 // GET          127.0.0.1:3333/scores/student?token=  - get scores by admin
-// GET          127.0.0.1:3333/scores/student/:id/attempts?token  - get total attempts score by admin
+// GET          127.0.0.1:3333/scores/student/:id/attempts?token=  - get total attempts score by admin
 router.route('/student').get(mustBeAdmin, getScores)
 router.route('/student/:id/attempts').get(mustBeAdmin, getUserAttempts)
 
